dfc5ead7ff5b915193bf4e79302d067e4091aec8
pone <pooo321@hotmail.com>
2018-05-29 15:16:43 +0800

    Rename img

--- server/update_jekyll.sh
+++ server/update_jekyll.sh
@@ -2,12 +2,9 @@
 
 __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 __date="$(date +%x)"
-# __upToDate="up-to-date"
-# __gitStatus="$(git status && grep ${__upToDate})"
 
 cd "${__dir}/../client"
 jekyll build
-# cd _site
 git pull origin master
 git add .
 git commit -m "Update data - auto push ${__date}"
